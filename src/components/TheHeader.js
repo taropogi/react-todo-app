ade0d2195edbbc364edb959c018eb0cdd656d66a
Richard Bernisca <taropogi_123@yahoo.com>
2023-01-01 01:52:00 +0800

    By RB

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -1,11 +1,10 @@
 import React from "react";
 
-// import "./App.css";
-
 const TheHeader = () => {
   return (
     <div className="header">
       <h1>Todo Lists</h1>
+      <p>by: Richard Bernisca</p>
     </div>
   );
 };
